662652a8e619ff5c6c3aafc94354e49c436ba34a
shi.yanxun <jy01095902@126.com>
2019-08-01 14:55:30 +0800

    #12 修改Dockerfile

--- main.go
+++ main.go
@@ -6,12 +6,18 @@ import (
 	"context"
 	"flag"
 	"log"
+	"time"
 )
 
 func main() {
 	flag.Parse()
 	factory.Init()
-	etl := services.InStorageETL{}.New("2019-07-01 00:00:00", "2019-07-31 23:59:59")
+
+	start := time.Now().Format("2006-01-02") + " 00:00:00"
+	end := time.Now().Format("2006-01-02") + " 23:59:59"
+	log.Printf("起始日期: %s, 结束日期: %s \n", start, end)
+
+	etl := services.InStorageETL{}.New(start, end)
 	err := etl.Run(context.Background())
 	if err != nil {
 		log.Println(err.Error())
