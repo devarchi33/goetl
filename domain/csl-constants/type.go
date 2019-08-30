0c3de99e5a644c0905863eceb28d83eb62ca7c4b
shi.yanxun <jy01095902@126.com>
2019-08-30 13:12:25 +0800

    #44 添加获取14天仍未入库的卖场分配出库单的逻辑

---
+++ domain/csl-constants/type.go
@@ -0,0 +1,12 @@
+package cslConstants
+
+const (
+	// TypLogisticsToShop 从物流到卖场，卖场分配
+	TypLogisticsToShop string = "01"
+
+	// TypFactoryToShop 从工厂到卖场，工厂直送
+	TypFactoryToShop string = "16"
+
+	// TypAncillaryProduct 销售辅助用品入库
+	TypAncillaryProduct string = "66"
+)
